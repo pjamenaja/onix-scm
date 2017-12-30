0ee825c84bf29d8996efc150ec4527c742b2cf49
Seubpong Monsar <pjame.fb@gmail.com>
2017-12-30 17:11:51 +0700

    WIP

--- init_server/wis_boot_script.bash
+++ init_server/wis_boot_script.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-USER=dt74698
+COMPANY=wintechthai
+IP=35.227.147.247
+
+USER=seubpong
 SSH_DIR=/home/${USER}/.ssh
 KEY_FILE=/home/${USER}/.ssh/authorized_keys
 PUB_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAqO1jMSJtkzsSrah9QDJD+QHqwXLNCf8C0+JOJeRb5L0dQnNjHhiuGQOOT6h6htaTg8T94I5e6Zf28O+R9/jl0pAeHjxN1ghwzqz5Qjr3fXjAsrbrUs4wEFx4FBMT9v1K/GG5TZxl0M+5UIuXM4j+Yjj0uWQUD3AVUwZbUPWuK2sUfRpE8ycFheIm77h5oCFkyAiHIhpk0wRSqfcX2tPqH23LdQVB06M35zPja8ydhFpzV21rb1h8C8GD33E0Nf0BAt6rdrU+A8PDjikMRRix8W5xhAGKwtfKrVjsovIRN8EdAw4eEUaSCWtSoCSGt1oxAFArEl4MHtaBJWk3dBl6vw== rsa-key-20171230'
+
+export WIS_KEY_FILE=/etc/httpd/ssl/${COMPANY}.key
+export WIS_CERT_FILE=/etc/httpd/ssl/${COMPANY}.crt
+export WIS_IP_ADDR=${IP}
 
 useradd ${USER}
 passwd ${USER}
--- init_server/wis_server_init.php
+++ init_server/wis_server_init.php
@@ -4,9 +4,12 @@
 */
 
 declare(strict_types=1);
-$_ENV['BIN_DIR'] = dirname(__FILE__);
 
-$COMMAND_LISTS = [
+$_ENV['BIN_DIR'] = dirname(__FILE__);
+$_ENV['WIS_KEY_FILE'] = getenv('WIS_KEY_FILE');
+$_ENV['WIS_CERT_FILE'] = getenv('WIS_CERT_FILE');
+
+$PHP_COMMAND_LISTS_7_1 = [
                      'rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm',
                      'rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm',
                      'yum install mod_php71w php71w-opcache',
@@ -16,47 +19,79 @@ $COMMAND_LISTS = [
                      'yum install php71w-fpm php71w-opcache',
                      'yum install php71w-common',
                      'yum install php71w-cli',
-
-                     'yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-1.noarch.rpm',
-                     'yum install postgresql10',
-                     'yum install postgresql10-server',
-                     '/usr/pgsql-10/bin/postgresql-10-setup initdb',
-
-                     'yum install httpd',
-                     'yum install mod_ssl', 
-                     'mkdir /wis',
-                     'chmod 777 /wis'                    
                 ];
 
-runCommands($COMMAND_LISTS);
+$APACHE_COMMAND_LISTS_2_4 = [
+                    'yum install httpd',
+                    'yum install mod_ssl',                    
+                    'mkdir /etc/httpd/ssl',
+                    "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $_ENV[WIS_KEY_FILE] -out $_ENV[WIS_CERT_FILE]",      
+                    'apache_func_call(httpd.conf_2.4)',
+                    'echo HelloWorld > /var/www/html/index.html',
+                ];
+
+$PGSQL_COMMAND_LISTS_10_0 = [
+                    'yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-1.noarch.rpm',
+                    'yum install postgresql10',
+                    'yum install postgresql10-server',
+                    '/usr/pgsql-10/bin/postgresql-10-setup initdb',
+                    'pgsql_func_call(pg_hba.conf_10.0)',                    
+                ];
+
+$GENERIC_COMMAND_LISTS_1_0 = [
+                    'mkdir /wis',
+                    'chmod 777 /wis',                                        
+                ];
+
+$PROFILE=[$PHP_COMMAND_LISTS_7_1, $APACHE_COMMAND_LISTS_2_4, $PGSQL_COMMAND_LISTS_10_0, $GENERIC_COMMAND_LISTS_1_0];
+
+runCommands($PROFILE);
 exit(0);
+
+function runCommands($profiles)
+{
+    foreach ($profiles as $arr)
+    {
+        foreach ($arr as $cmd)
+        {
+            print("==== Executing [$cmd]...\n");
+
+            if (preg_match_all('/^(.*)_func_call\((.*)\)$/', $cmd, $matches))
+            {
+                $func_key = $matches[1][0];
+                $param = $matches[2][0];
+
+                print("Calling function [$func_key] with param [$param]\n");
+                if ($func_key == 'apache')
+                {
+                    configSslApache($param);
+                }
+                else if ($func_key == 'pgsql')
+                {
+                    configPostgreSQL($param);
+                }                
+            }
+            else
+            {
+                exec($cmd, $outputs, $retCode);
+            }
+        }
+    }    
+}
 
 function configSELinux()
 {
     print("==== Configuring SELinux ...\n");
 }
 
-function configPostgreSQL()
+function configPostgreSQL($template)
 {
     print("==== Configuring PostgreSQL ...\n");
 }
 
-function configSSL()
+function configSslApache()
 {
     print("==== Configuring SSL ...\n");
-}
-
-function runCommands($cmds)
-{
-    foreach ($cmds as $cmd)
-    {
-        print("==== Executing [$cmd]...\n");
-        exec($cmd, $outputs, $retCode);
-        //if ($retCode)
-        //{
-        //    throw new Exception('Error : ' . $outputs[0]);
-        //}    
-    }    
 }
 
 ?>
