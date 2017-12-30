4e09c1ea929ae362d50e48449722606f44ef46ac
Seubpong Monsar <pjame.fb@gmail.com>
2017-12-30 13:49:12 +0700

    Created automated script to intialize server

---
+++ init_server/wis_boot_script.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER=dt74698
+SSH_DIR=/home/${USER}/.ssh
+KEY_FILE=/home/${USER}/.ssh/authorized_keys
+PUB_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAqO1jMSJtkzsSrah9QDJD+QHqwXLNCf8C0+JOJeRb5L0dQnNjHhiuGQOOT6h6htaTg8T94I5e6Zf28O+R9/jl0pAeHjxN1ghwzqz5Qjr3fXjAsrbrUs4wEFx4FBMT9v1K/GG5TZxl0M+5UIuXM4j+Yjj0uWQUD3AVUwZbUPWuK2sUfRpE8ycFheIm77h5oCFkyAiHIhpk0wRSqfcX2tPqH23LdQVB06M35zPja8ydhFpzV21rb1h8C8GD33E0Nf0BAt6rdrU+A8PDjikMRRix8W5xhAGKwtfKrVjsovIRN8EdAw4eEUaSCWtSoCSGt1oxAFArEl4MHtaBJWk3dBl6vw== rsa-key-20171230'
+
+useradd ${USER}
+passwd ${USER}
+
+TEMP_PUB_FILE=./pubkey.pub
+
+sudo -u ${USER} mkdir ${SSH_DIR}
+sudo -u ${USER} chmod 700 ${SSH_DIR}
+echo ${PUB_KEY} > ${TEMP_PUB_FILE}
+cp ${TEMP_PUB_FILE} ${KEY_FILE}
+chown ${USER}:${USER} ${KEY_FILE}
+chmod 600 ${KEY_FILE}
+rm ${TEMP_PUB_FILE}
+
+yum install php71w-fpm php71w-opcache
+yum install php71w-common
+yum install php71w-cli
+
+php wis_server_init.php
--- init_server/wis_server_init.php
+++ init_server/wis_server_init.php
@@ -16,27 +16,46 @@ $COMMAND_LISTS = [
                      'yum install php71w-fpm php71w-opcache',
                      'yum install php71w-common',
                      'yum install php71w-cli',
+
                      'yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-1.noarch.rpm',
                      'yum install postgresql10',
                      'yum install postgresql10-server',
+                     '/usr/pgsql-10/bin/postgresql-10-setup initdb',
+
                      'yum install httpd',
                      'yum install mod_ssl', 
                      'mkdir /wis',
                      'chmod 777 /wis'                    
-                    ];
+                ];
 
-runCommands();
+runCommands($COMMAND_LISTS);
 exit(0);
 
-function runCommands()
+function configSELinux()
 {
-    foreach ($COMMAND_LISTS as $cmd)
+    print("==== Configuring SELinux ...\n");
+}
+
+function configPostgreSQL()
+{
+    print("==== Configuring PostgreSQL ...\n");
+}
+
+function configSSL()
+{
+    print("==== Configuring SSL ...\n");
+}
+
+function runCommands($cmds)
+{
+    foreach ($cmds as $cmd)
     {
+        print("==== Executing [$cmd]...\n");
         exec($cmd, $outputs, $retCode);
-        if ($retCode)
-        {
-            throw new Exception('Error : ' . $outputs[0]);
-        }    
+        //if ($retCode)
+        //{
+        //    throw new Exception('Error : ' . $outputs[0]);
+        //}    
     }    
 }
 
