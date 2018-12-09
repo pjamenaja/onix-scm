de1b8ca551eb78022b608203fe5e1604fb7ddf04
root <root@onix-kafka-server.c.compute-engine-vm-test.internal>
2018-12-09 13:16:46 +0000

    No Y/N confirmation

--- init_server/wis_boot_script.bash
+++ init_server/wis_boot_script.bash
@@ -17,14 +17,14 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
 #yum install php72w-fpm --- NO NEED 
-yum install php72w-common
-yum install php72w-cli
+yum -y install php72w-common
+yum -y install php72w-cli
 yum -y install php72w-pdo
-yum install php72w-mbstring
-yum install php72w-gd
-yum install wget
+yum -y install php72w-mbstring
+yum -y install php72w-gd
+yum -y install wget
 
-php wis_server_init.php
+#php wis_server_init.php
 
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
